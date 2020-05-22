7677b7efb78cbaab5ac300b83232a59833900796
Hardik.2852 <57672741+Hardik7212@users.noreply.github.com>
2020-05-22 08:03:30 +0530

    Add files via upload
    Footer Added

---
+++ footer.php
@@ -0,0 +1,14 @@
+<!-- Footer -->
+<footer class="page-footer font-small cyan darken-3">
+
+
+  
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
